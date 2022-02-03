b4bf4216bd4fbdde6645691b294632bcdb674eb9
Cathy T Bradley <cathytbradley116@gmail.com>
2022-02-03 14:28:00 -0800

    make database
    Co-authored-by: Naria Nexis <nnanimefreak5@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
 
-//     $('.collapse').collapse({
-//       toggle: true
-//     })
+});
 
 
-// });
+
+
+const hadesDatabase = [
+  {
+    prophecy: "Fully Loaded",
+    requirements: [
+      {
+        boon: "Support Fire",
+        needs: ["Deadly Strike", "Deadly Flourish", "True Shot", "Hunter Dash", "Artemis' Aid", "Pressure Points"]
+      }
+    ]
+  },
+  {
+    prophecy: "Divine Protection",
+    requirements: [
+      {
+        boon: "Brilliant Riposte",
+        needs: ["Divine Strike", "Divine Flourish", "Divine Dash", "Holy Shield"]
+      }
+    ]
+  }
+]
