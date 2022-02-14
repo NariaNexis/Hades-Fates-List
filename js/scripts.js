8e0bf1240b0f0f48ca0ebae3f688ede5fdb029f0
Naria Nexis <nnanimefreak5@gmail.com>
2022-02-14 14:32:37 -0800

    add basic boons

--- js/scripts.js
+++ js/scripts.js
@@ -73,7 +73,7 @@ const boons = {
      },
   1: {
       boon: "Exit Wounds",
-      requiredNeeds: [1,6]
+      requiredNeeds: [2,5,6,8,10]
      },
   2: {
       boon: "Pressure Points",
@@ -81,21 +81,56 @@ const boons = {
      },
   3: {
       boon: "Support Fire",
-      requiredNeeds: [1,2,3,4,5]
+      requiredNeeds: [1,3,4,7,9,10]
      },
   4: {
       boon: "Brilliant Reposte",
-      requiredNeeds: [0]
+      requiredNeeds: [11,12,13,14]
+     },
+  5: {
+      boon: "Black Metal/Engulfing Vortex",
+      requiredNeeds: [15,16,17,18]
+     },
+  6: {
+      boon: "Greater Recall",
+      requiredNeeds: [20,21,22,23]
+     },
+  7: {
+      boon: "Bad News",
+      requiredNeeds: [19,22,24]
      }
 };
 
 
 const needs = {
   0: "none",
-  1: "deadly strike",
-  2: "True Shot",
-  3: "Hunter Dash",
-  4: "Artemis's Aid",
-  5: "Pressure Points",
-  6: "Crushing shot"
+  1: "Artemis' Aid",
+  2: "Crush Shot",
+  3: "Deadly Flourish",
+  4: "Deadly Strike",
+  5: "Electric Shot",
+  6: "Flood Shot",
+  7: "Hunter Dash",
+  8: "Phalanx Shot",
+  9: "Pressure Points",
+  10: "True Shot",
+  11: "Divine Dash",
+  12: "Divine Flourish",
+  13: "Divine Strike",
+  14: "Holy Shield",
+  15: "Ares' Aid",
+  16: "Blade Dash",
+  17: "Slicing Flare",
+  18: "Slicing Shot",
+  19: "Auto Reload",
+  20: "Flurry Cast",
+  21: "Infernal Soul Mirror",
+  22: "Lambent Plume keepsake",
+  23: "Quick Reload",
+  24: "Stygian Soul Mirror",
+  25: "Demeter's Aid",
+  26: "Frost Flourish",
+  27: "Frost Strike",
+  28: "Mistral Dash",
+  29: "Snow Burst"
 };
